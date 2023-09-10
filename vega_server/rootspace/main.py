f7b634428e0527bc6818c1150c61e327b7e5fa16
eerbano <enzo.erbano@gmail.com>
2023-09-09 22:24:35 -0400

    vega_server rootspace primary files

---
+++ vega_server/rootspace/main.py
@@ -0,0 +1,22 @@
+
+from threading import Thread, Event
+
+import server.svRootThread as svRootThread
+import cpuclocking.cpuClockingThread as ccThread
+
+import globals
+import gpucooler.gpuThread as gtThread
+globals.init()
+
+if __name__ == '__main__':
+
+    v = Thread(target=ccThread.cpuclocking_thread, args=(1,))
+    v.start()
+
+    z = Thread(target=gtThread.gpu_thread, args=(1,))
+    z.start()
+    # z.join()
+    
+    y = Thread(target=svRootThread.server_thread, args=(1,))
+    y.start()
+    # y.join()
