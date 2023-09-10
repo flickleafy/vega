d7c1ee14b7667ed673aabbb222e3471eb35872fe
eerbano <enzo.erbano@gmail.com>
2023-09-09 22:25:22 -0400

    vega_server userspace primary files

---
+++ vega_server/userspace/main.py
@@ -0,0 +1,20 @@
+
+from threading import Thread, Event
+
+import server.svUserThread as svUserThread
+import lighting.lightingThread as ltThread
+import watercooler.wcThread as wcThread
+
+import globals
+globals.init()
+
+if __name__ == '__main__':
+
+    x = Thread(target=wcThread.watercooler_thread, args=(1,))
+    x.start()
+
+    y = Thread(target=svUserThread.server_thread, args=(1,))
+    y.start()
+
+    w = Thread(target=ltThread.lighting_thread, args=(1,))
+    w.start()
