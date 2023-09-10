aa0d061b9b7604a9d0bd48199370b26e6945bf8a
eerbano <enzo.erbano@gmail.com>
2023-09-09 22:31:03 -0400

    server thread for gateway

---
+++ vega_server/userspace/server/svUserThread.py
@@ -0,0 +1,21 @@
+import globals
+
+import socket
+
+from server.start_server import start_server
+
+
+def server_thread(_):
+    """_summary_
+
+    Args:
+        _ (_type_): _description_
+
+    Returns:
+        null: simple thread with no returns
+    """
+    host = socket.gethostname()   # get local machine name
+    port = 9095  # > 1024 $$ <65535 range
+    server_name = "Userspace Server"
+
+    start_server(host, port, server_name, globals.WC_DATA_OUT)
