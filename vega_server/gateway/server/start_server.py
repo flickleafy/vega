e0a21c2db73c8b85e10e616df1f124fbbc0de13c
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-02-18 15:27:48 -0400

    multiclient handling

--- vega_server/gateway/server/start_server.py
+++ vega_server/gateway/server/start_server.py
@@ -9,36 +9,31 @@ def start_server(address, port, server_name, send_data_1, send_data_2):
     """
     Start the server and listen for incoming connections.
     """
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((address, port))
         server_socket.listen()
         print(f"{server_name} started. Waiting for connections...")
         while True:
-            try:
-                connection, addr = server_socket.accept()
-                print(f"Connection from {addr}")
-                client_thread = threading.Thread(
-                    target=handle_client, args=(connection, send_data_1, send_data_2))
-                client_thread.start()
-            except Exception as e:
-                print(f"An error occurred with a client: {e}")
-    except Exception as e:
-        print(f"An error occurred while starting the server: {e}")
-    finally:
-        server_socket.close()
+            connection, addr = server_socket.accept()
+            print(f"Connection from {addr}")
+            client_thread = threading.Thread(target=handle_client, args=(
+                connection, addr, send_data_1, send_data_2))
+            client_thread.daemon = True  # Optional: make client threads daemon threads
+            client_thread.start()
 
 
-def handle_client(connection, send_data_1, send_data_2):
+def handle_client(connection, address, send_data_1, send_data_2):
     """
     Handle the client connection.
     """
     try:
-        if not data_transfer_loop(connection, send_data_1, send_data_2):
-            connection.close()
+        print(f"Handling client {address}")
+        data_transfer_loop(connection, send_data_1, send_data_2)
     except Exception as e:
-        print(f"An error occurred with the connection: {e}")
+        print(f"Error handling client {address}: {e}")
+    finally:
         connection.close()
+        print(f"Connection with {address} closed")
 
 
 def data_transfer_loop(connection, send_data_1, send_data_2):
@@ -46,18 +41,13 @@ def data_transfer_loop(connection, send_data_1, send_data_2):
     Handle data transfer for the connection.
     """
     while True:
-        try:
-            json_data_in = connection.recv(1024)
-            if not json_data_in:
-                print("Connection lost. Trying to reconnect...")
-                return False
+        json_data_in = connection.recv(1024)
+        if not json_data_in:
+            print("Connection lost.")
+            break  # Exit loop and function, leading to thread termination
 
-            json_data_in = json_data_in.decode('utf-8')
-            json_data_out = json.dumps({**send_data_1[0], **send_data_2[0]})
-            print(get_current_time() +
-                  "Sending to gateway server: ", str(json_data_out))
-            connection.sendall(str(json_data_out).encode('utf-8'))
-        except Exception as e:
-            print(f"An error occurred in data transfer: {e}")
-            return False
+        json_data_in = json_data_in.decode('utf-8')
+        json_data_out = json.dumps({**send_data_1[0], **send_data_2[0]})
+        print(get_current_time() + "Sending to client: ", str(json_data_out))
+        connection.sendall(json_data_out.encode('utf-8'))
         time.sleep(3)
